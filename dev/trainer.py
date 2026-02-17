ff0d5f210e9f91edcda4bcd436cd28e4ef3a8d09
hacker19232 <akashyap164@student.fuhsd.org>
2026-02-16 22:46:41 -0800

    evaluate method completed

--- dev/trainer.py
+++ dev/trainer.py
@@ -11,16 +11,18 @@ from utils.hook import AttentionExtractor
 import numpy as np
 from utils.logger import Logger
 from utils.hook import AttentionExtractor
+from GABRIL_utils import utils
+from GABRIL_utils.atari_env_manager import create_env as create_atari_environment
 
 class Trainer:
     def __init__(self, config, train_loader, val_loader):
         self.cfg = config
         self.device = torch.device('cuda' if torch.cuda.is_available() else 'cpu') # not in yaml bc its not gna change
-
         self._construct_components(train_loader, val_loader)
         
 
     def _construct_components(self, train_loader, val_loader):
+        self.env = create_atari_environment(self.cfg.env_name) # not in yaml bc its not gna change, also we only need env for evaluation, so maybe should move this to evaluate() method instead of init
         self.loss_fn = instantiate(self.cfg.trainer.loss)
         self.model = instantiate(self.cfg.trainer.model).to(self.device)
         self.attn_extractor = AttentionExtractor()
@@ -70,7 +72,38 @@ class Trainer:
 
             self.logger.log_scalar_dict(loss_dict, step=epoch) # log last timestep in the epoch
             
+            if epoch % self.cfg.evaluation.eval_interval == 0:
+                self.evaluate(epoch)
+
             print(f"Epoch {epoch} done!")
 
-    def evaluate(self):
-        pass
+    def evaluate(self, step = 0):
+
+        self.model.eval() 
+        num_eval_episodes = self.cfg.evaluation.num_eval_episodes
+        rewards = [0 for i in range(num_eval_episodes)]
+
+        with torch.no_grad():
+            for episode in range(num_eval_episodes):
+                obs, _ = self.env.reset()
+                done = False
+
+                while not done:
+                    obs_tensor = torch.as_tensor(obs, dtype=torch.float32).unsqueeze(0).to(self.device)/255.0 # add batch dim
+
+                    
+                    action_pred = self.model(obs_tensor)
+
+                    action = torch.argmax(action_pred, dim=-1).item() # get the predicted action index
+                    obs, reward, terminated, truncated, _ = self.env.step(action)
+                    done = terminated or truncated
+                    rewards[episode] += reward
+                    
+                    
+
+
+        avg_reward = np.mean(rewards)   
+        self.logger.log_scalar_dict({'avg_eval_reward': avg_reward}, step=step)
+        self.model.train()
+        
+

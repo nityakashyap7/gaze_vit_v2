0ccdff6f198f8de32400ad8b028831853f92bc9b
nityakashyap7 <nityakas@usc.edu>
2026-02-27 11:32:47 -0800

    add the loss function as a parameter for init

--- dev/losses/ce_plus_gaze_reg.py
+++ dev/losses/ce_plus_gaze_reg.py
@@ -1,13 +1,15 @@
 import torch.nn.functional as F
 
 class CEPlusGazeReg: 
-    def __init__(self, reg_lambda=1.0):
+    def __init__(self, reg_loss_fn, reg_lambda=1.0):
+        self.reg_loss_fn = reg_loss_fn
         self.reg_lambda = reg_lambda
+        
 
 
-    def __call__(self, action_preds, action_targs, gaze_preds, gaze_targs, reg_loss_fn, **kwargs):
+    def __call__(self, action_preds, action_targs, gaze_preds, gaze_targs, **kwargs):
         ce = F.cross_entropy(action_preds, action_targs)
 
-        reg = self.reg_lambda * reg_loss_fn(gaze_preds, gaze_targs)
+        reg = self.reg_lambda * self.reg_loss_fn(gaze_preds, gaze_targs)
 
         return ce + reg
